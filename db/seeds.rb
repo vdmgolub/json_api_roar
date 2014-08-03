92469ce8e1c4e3d4ebe72f6eb29b6198b87da99e
Vadim Golub <vdmgolub@gmail.com>
2014-08-03 16:10:08 +0300

    DB:seed rake task updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-10.times do |index|
-  Post.create(text: "lorem ipsum #{index}")
+Post.delete_all
+Author.delete_all
+
+['Jay', 'Silent Bob'].each do |name|
+  author = Author.create(name: name)
+
+  10.times do |index|
+    Post.create(text: "lorem ipsum #{index}", author: author)
+  end
 end
